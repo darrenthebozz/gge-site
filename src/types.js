84be91dca7704bbf38a3947abfc4c617d2577a9d
darrenthebozz <dazwallcroft123@gmail.com>
2025-12-12 03:03:20 +0000

    Fix server selection bug

--- src/types.js
+++ src/types.js
@@ -44,7 +44,7 @@ class User {
       this.plugins = obj.plugins
       this.lt = Number(obj.lt)
       this.externalEvent = Boolean(obj?.externalEvent)
-      this.server = Boolean(obj?.server)
+      this.server = Number(obj?.server)
     }
   }
 export { ActionType, GetActionTypeName, ErrorType, GetErrorTypeName, LogLevel, User }
